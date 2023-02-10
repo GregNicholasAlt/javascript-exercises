f430f17f9ddacea0127b7fdae0fa8e75fcc19a98
GregNicholasAlt <103330905+GregNicholasAlt@users.noreply.github.com>
2023-02-10 00:10:39 +0000

    summAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(s,e) {
+    if(![s,e].every(el => typeof el === 'number' && el >= 0)){
+        return 'ERROR'
+    }
+    if (e<s){
+        let temp = e;
+        e = s;
+        s = temp;
+    }
+    let sum = 0
+    for(let i=s; i<=e; i++){
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
