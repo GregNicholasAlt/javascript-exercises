60a100a8e3901477b9996889ac16ae0be41dd3be
GregNicholasAlt <103330905+GregNicholasAlt@users.noreply.github.com>
2023-02-10 00:03:00 +0000

    fromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...rest) {
+    let ans = [...arr]
+    rest.forEach(arg => {
+        ans = ans.filter(el => el !== arg)
+    })
+    return ans
 };
 
 // Do not edit below this line
